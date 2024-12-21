48a00692348cf9635726fa526302c12bbd3a8675
Marcin Mika <marcin.mika123@gmail.com>
2024-12-21 22:51:36 +0100

    05_sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (leftNum, rightNum) {
+  let sum = 0;
+  let smallerNum = 0;
+  let higherNum = 0;
 
+  if (leftNum >= 0 && rightNum >= 0 && Number.isInteger(leftNum) && Number.isInteger(rightNum)) {
+    if (leftNum < rightNum) {
+      smallerNum = leftNum;
+      higherNum = rightNum;
+    } else {
+      smallerNum = rightNum;
+      higherNum = leftNum;
+    }
+
+    for (let i = smallerNum; i <= higherNum; i++) {
+      sum += i;
+    }
+    return sum;
+  } else return 'ERROR';
 };
 
 // Do not edit below this line
