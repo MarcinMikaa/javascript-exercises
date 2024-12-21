86578e38e4156ea0253d64c6a128f4ecb30aecb3
Marcin Mika <marcin.mika123@gmail.com>
2024-12-21 23:50:55 +0100

    09_palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (wordToCheck) {
+  let cleanedWord = wordToCheck.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  let reversedWord = cleanedWord.split('').reverse().join('');
 
+  return cleanedWord === reversedWord;
 };
 
 // Do not edit below this line
