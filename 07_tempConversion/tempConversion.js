35a1a0a30f0b7b4fef2e2f7304e4cb19648385b7
Marcin Mika <marcin.mika123@gmail.com>
2024-12-21 23:18:33 +0100

    07_tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const fahrenheitToCelsius = ((fahrenheit - 32) * 5) / 9;
+  return parseFloat(fahrenheitToCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const celsiusToFahrenheit = (celsius * 9) / 5 + 32;
+  return parseFloat(celsiusToFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
