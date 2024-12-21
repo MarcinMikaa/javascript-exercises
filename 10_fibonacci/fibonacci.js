1073363832ef42c05c185c4362e9da5f9c9c26c6
Marcin Mika <marcin.mika123@gmail.com>
2024-12-21 23:56:25 +0100

    10_fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  const n = parseInt(input, 10);
 
+  if (isNaN(n) || n < 0) {
+    return 'OOPS';
+  }
+
+  if (n === 0) {
+    return 0;
+  }
+
+  let prev = 0,
+    curr = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = curr;
+    curr = curr + prev;
+    prev = temp;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
